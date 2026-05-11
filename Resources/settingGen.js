a5373f07a02e5d307456e731bc2ef76a9642d35b
Kameron Kriens <kameronkriens@Kamerons-MacBook.local>
2026-05-11 00:07:05 -0500

    settings broken right now continue 5/11 (night)

--- Resources/settingGen.js
+++ Resources/settingGen.js
@@ -13,17 +13,33 @@ class setting {
                 throw new Error("Invalid type: " + this.type)
             }
 
+            // verifying there are no duplicates
+
+            if(document.getElementById(this.value) != null) throw new Error("Duplicate ID: " + this.value)
+    try{
             if (this.type == 'bool') {
                 let obj = /* html */ `
-            <div class="settingObj" id="${this.value}">
-                <img src="Resources/svg/setting/${this.value}.svg" class="settingIcon">
-            </div>
+                <button id="${this.value}">
+                    hola buenas
+                </button>
             `
                 document.getElementById('baseContent').innerHTML += obj
 
+                document.getElementById(this.value).addEventListener('click', () => {
+                   alert('This setting is not yet implemented!')
+                })
+
+                console.log(document.getElementById(this.value)?.onclick)
+                console.log(this.value)
+                console.log('hello?')
             }
 
+    }catch(e){
+        console.warn(e)
+    }
     }
 }
 
 window.setting = setting
+
+let testSetting = new setting('test', 'off', 'bool', 'This is a test setting')
