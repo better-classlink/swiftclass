57a028ea5e7b6f4dd7f7f8be9790417b00ca19a3
better-classlink <krienskam@gmail.com>
2026-05-16 12:27:39 -0500

    tweaks

--- Resources/info/infoSlides.js
+++ Resources/info/infoSlides.js
@@ -1,9 +1,14 @@
-class InfoSlides(sourceFile){
+class InfoSlides{
+
     constructor(sourceFile){
         this.sourceFile = sourceFile;
     }
+
     display(){
-        this.slideNumber = 0
-        this.slides = JSON.parse(this.sourceFile)
+        let slideNumber = 0
+        let slides = fetch (this.sourceFile).then(() => {
+            return fetch(this.sourceFile).then(response => response.json())
+        })
+        print('slides: ' + slides)
     }
 }
